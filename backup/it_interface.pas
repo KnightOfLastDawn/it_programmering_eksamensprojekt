2fa6b9188b14eff5ac8bbe449aeee7dd65d9987e
KnightOfLastDawn <36958112+KnightOfLastDawn@users.noreply.github.com>
2019-06-18 10:46:10 +0200

    made a button that shows a message with hello world

--- backup/it_interface.pas
+++ backup/it_interface.pas
@@ -72,12 +72,11 @@ begin
   RunProgram.CommandLine := 'python3 ./bestemmelse_af_rod_med_textfil_og_graf_v1.py';
   RunProgram.Execute;
   RunProgram.Free;
-  ShowMessage('burde virke?');
 end;
 
 procedure TForm1.Button4Click(Sender: TObject);
 begin
-
+  ShowMessage ('test');
 end;
 
 
